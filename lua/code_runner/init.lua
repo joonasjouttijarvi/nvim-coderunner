54f3c9b325197b10f3105d4b98df9e5a7c5697e6
joonasjouttijarvi <joonas.jouttijarvi@gmail.com>
2023-10-25 17:04:14 +0300

    init

---
+++ lua/code_runner/init.lua
@@ -0,0 +1,31 @@
+M.run_code = function()
+    local file_extension = vim.fn.expand('%:e') -- Get file extension
+    local filename = vim.fn.expand('%:p')
+
+    local cmd = ""
+
+    if file_extension == "py" then
+        cmd = "python3 " .. filename
+    elseif file_extension == "js" or file_extension == "jsx" or file_extension then
+        cmd = "node " .. filename
+    elseif file_extension == "java" then
+        local options = {
+            "1. javac",
+            "2. maven"
+        }
+        local choice = vim.fn.inputlist(options)
+        if choice == 1 then
+            cmd = "javac " .. filename .. " && java " .. vim.fn.expand('%:r')
+        elseif choice == 2 then
+            cmd = "mvn compile exec:java"
+        else
+            return
+        end
+    end
+
+    if cmd ~= "" then
+        vim.cmd(":belowright split | resize 15 | terminal " .. cmd)
+    else
+        print("No runner configured for this file extension.")
+    end
+end

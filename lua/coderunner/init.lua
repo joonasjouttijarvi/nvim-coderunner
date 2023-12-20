9f3bb894629dfbe82ed0c3e6c2f3f4242be85114
Joonas Jouttijärvi <joonas@Fnetti.local>
2023-12-20 17:13:23 +0200

    runner for c++

--- lua/coderunner/init.lua
+++ lua/coderunner/init.lua
@@ -3,23 +3,25 @@ local M = {}
 local js_extensions = { "js" }
 
 M.run_code = function()
-    local file_extension = vim.fn.expand('%:e') -- Get file extension
-    local cmd = ""
+	local file_extension = vim.fn.expand("%:e") -- Get file extension
+	local cmd = ""
 
-    if file_extension == "py" then
-        cmd = require('coderunner.python').get_command()
-    elseif file_extension == "java" then
-        cmd = require('coderunner.java').get_command()
-    elseif file_extension == js_extensions then
-        cmd = require('coderunner.node').get_command()
-    end
+	if file_extension == "py" then
+		cmd = require("coderunner.python").get_command()
+	elseif file_extension == "java" then
+		cmd = require("coderunner.java").get_command()
+	elseif file_extension == js_extensions then
+		cmd = require("coderunner.node").get_command()
+	elseif file_extension == "cpp" then
+		cmd = require("coderunner.cpp").get_command()
+	end
 
-    if cmd ~= "" then
-        -- Open terminal in a split and run the command
-        vim.cmd(":belowright split | resize 15 | terminal " .. cmd)
-    else
-        print("No runner configured for this file extension.")
-    end
+	if cmd ~= "" then
+		-- Open terminal in a split and run the command
+		vim.cmd(":belowright split | resize 15 | terminal " .. cmd)
+	else
+		print("No runner configured for this filetype")
+	end
 end
 
 return M
